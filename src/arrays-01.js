8f8b113af61d43645a5f6c4995019647ecb0f584
Gokul Ram <52551290+agokulram1@users.noreply.github.com>
2023-02-20 20:33:15 -0500

    did arrays-01

--- src/arrays-01.js
+++ src/arrays-01.js
@@ -25,7 +25,7 @@
  * ? https://bit.ly/3EUI0j9 - Reading and Writing Array Elements
  */
 function getFirstValue(arr) {
-  // write your    code here & return value
+  return arr[0];
 }
 
 /**
@@ -37,7 +37,12 @@ function getFirstValue(arr) {
  * @returns {array} - array of the 4 elements
  */
 function makeArray(a, b, c, d) {
-  // write your code here & return value
+  const arr = new Array();
+  arr[0] = a;
+  arr[1] = b;
+  arr[2] = c;
+  arr[3] = d;
+  return arr;
 }
 
 /**
@@ -48,7 +53,8 @@ function makeArray(a, b, c, d) {
  *
  */
 function createFirstFivePrimes() {
-  // write your code here & return value
+  const array = [2, 3, 5, 7, 11];
+  return array;
 }
 /**
  * Creates a new array with a number of elements
@@ -59,9 +65,12 @@ function createFirstFivePrimes() {
  * ? must use a for loop
  */
 function createSizedArray(size) {
-  // write your code here & return value
+  const arr = new Array(size);
+  for (let i = 0; i < size; i++) {
+    arr[i] = i;
+  }
+  return arr;
 }
-
 /**
  * Modifies the array by adding the number 1
  * both to the front and the back of the array
@@ -70,7 +79,9 @@ function createSizedArray(size) {
  * ? https://bit.ly/39z4FTE - Adding and Deleting Array Elements
  */
 function modifyArrayByAdding(arr) {
-  // write your code here & return value
+  arr.unshift(1);
+  arr.push(1);
+  return arr;
 }
 
 /**
@@ -80,7 +91,9 @@ function modifyArrayByAdding(arr) {
  * ? example: [1, 2, 3, 4, 5] => [2, 3, 4]
  */
 function modifyArrayByDeleting(arr) {
-  // write your code here & return value
+  arr.shift(1);
+  arr.pop(1);
+  return arr;
 }
 
 /**
@@ -91,7 +104,8 @@ function modifyArrayByDeleting(arr) {
  * ? example: [1, 2, 3, 4, 5], index = 2 => 3
  */
 function findElementAtIndex(arr, index) {
-  // write your code here & return value
+  const x = arr[index];
+  return x;
 }
 
 /**
@@ -103,7 +117,7 @@ function findElementAtIndex(arr, index) {
  * ? hint: https://bit.ly/2ZyFaAj - Array Searching and Sorting Methods
  */
 function findElementByValue(arr, value) {
-  // write your code here & return value
+  const x = arr.indexOf(value);
 }
 
 /**
@@ -115,7 +129,7 @@ function findElementByValue(arr, value) {
  * ? example: [1, 2, 3, 4, 5], index = 2, value = 0 => [1, 2, 0, 4, 5]
  */
 function changeElementAtIndex(arr, index, value) {
-  // write your code here & return value
+
 }
 
 /**
